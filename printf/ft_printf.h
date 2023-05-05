b634929c5c7b3fcc3b6049c69d25d70c9df8c2b8
David Machacek <dmachace@c2r5s4.42prague.com>
2023-05-05 15:08:26 +0200

    moved printf

---
+++ printf/ft_printf.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmachace <dmachace@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 10:26:33 by dmachace          #+#    #+#             */
+/*   Updated: 2023/03/17 15:08:20 by dmachace         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+void	ft_printf_putchar(int c, int *len);
+void	ft_printf_putstr(char *s, int *len);
+void	ft_nbr(int n, int *len);
+void	ft_nbr_unsig(unsigned int n, int *len);
+void	ft_conv_hex(unsigned long long n, char form, int *len);
+void	ft_void_point(unsigned long long n, int *len);
+void	ft_form_check(va_list *args, char form, int *len);
+int		ft_printf(const char *form, ...);
+
+#endif
